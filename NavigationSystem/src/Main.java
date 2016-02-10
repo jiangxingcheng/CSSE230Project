c8d45c3be4f2c12128c4b099b8f081af9d88ea36
jiangxingcheng <jiangx1@rose-hulman.edu>
2016-02-10 15:07:50 -0500

    no message

--- NavigationSystem/src/Main.java
+++ NavigationSystem/src/Main.java
@@ -1,25 +1,14 @@
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
+/**
+ * 
+ * Main class
+ *
+ * @author Xingcheng Jiang.
+ *         Created Feb 10, 2016.
+ */
 public class Main {
-	
-	public static void main(String[] args){
-		JFrame myFrame = new JFrame("Navigation System");
-		myFrame.setSize(400, 500);
-		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		myFrame.setVisible(true);
-		
-		JPanel panel = new JPanel();
-		myFrame.add(panel);
-		JButton button = new JButton("start");
-		panel.add(button);
-		//button.addActionListener (new Action1());
-
-		JButton button2 = new JButton("map");
-		panel.add(button2);
-		//button.addActionListener (new Action2()); 
-		
+	public static void main(String[] args) {
+		startFrame start = new startFrame();
+		start.startWindow();
 	}
 
 }
