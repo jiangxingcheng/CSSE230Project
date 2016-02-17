160692cc5b0f594b82fbc8bbc45878a693211de4
masterOfShadows <zamanmm@rose-hulman.edu>
2016-02-17 16:39:50 -0500

    Revert "Update mapLocation.java"
    This reverts commit 2ec8c66d1a7387ae62a2b3492947f1ff22b5e24f.

--- NavigationSystem/src/mapLocation.java
+++ NavigationSystem/src/mapLocation.java
@@ -5,21 +5,20 @@ public class mapLocation
 {
 
     String name;
-    double xCoordinate;
-    double yCoordinate;
+    public static int xCoordinate;
+    public static int yCoordinate;
     int interestRating; // Takes values 0 to 10, depending on how interesting a certain location is.
     ArrayList<String> relations;
 
-    public mapLocation(String name, double x, double y, int interestRating, ArrayList<String> relations)
+    public mapLocation(String name, int x, int y, int interestRating, ArrayList<String> relations)
     {
         this.name = name;
         this.interestRating = interestRating;
-        xCoordinate = x;
-        yCoordinate = y;
+        this.xCoordinate = x;
+        this.yCoordinate = y;
         this.relations = relations;
     }
-
-    public void addRelation(String location)
+	public void addRelation(String location)
     {
         this.relations.add(location);
     }
@@ -30,12 +29,12 @@ public class mapLocation
         return name;
     }
 
-    public double getX()
+    public int getX()
     {
         return xCoordinate;
     }
 
-    public double getY()
+    public int getY()
     {
         return yCoordinate;
     }
