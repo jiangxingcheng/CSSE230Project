7c4e77e74d0b08cfc56531cc2d822b8705c85fd4
jiangxingcheng <jiangx1@rose-hulman.edu>
2016-02-17 15:40:44 -0500

    Update startFrame.java

--- NavigationSystem/src/startFrame.java
+++ NavigationSystem/src/startFrame.java
@@ -1,121 +1,135 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
 /**
+ * 
  * Create a start frame with buttons have actionListener
  *
  * @author Xingcheng Jiang.
  *         Created Feb 10, 2016.
  */
-public class startFrame
-{
+public class startFrame {
+	
+	public void startWindow() {
+		JFrame myFrame = new JFrame("Navigation System");
+		
+		
+		myFrame.setSize(400, 500);
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    public void startWindow()
-    {
-        JFrame myFrame = new JFrame("Navigation System");
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.BLUE);
+		myFrame.add(panel);
 
+		JButton button = new JButton("start");
+		button.setBackground(Color.WHITE);
+		Font f = new Font(null, Font.ITALIC,16);
+		button.setFont(f);
+		panel.setLayout(null);
+		panel.add(button);
+		button.setBounds(300, 250, 70, 35);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Execute when button is pressed
+				System.out.println("Let's start");
+			}
+		});
 
-        myFrame.setSize(400, 500);
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JButton button2 = new JButton("map");
+		button2.setBackground(Color.WHITE);
+		button2.setFont(f);
+		panel.add(button2);
+		button2.setBounds(300, 300, 70, 35);
+		button2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Execute when button is pressed
+				System.out.println("Show map");
+				MapFrame myMap = new MapFrame();
+				myMap.startWindow();
+			}
+		});
 
-        JPanel panel = new JPanel();
-        myFrame.add(panel);
+		JButton button3 = new JButton("Reset");
+		button3.setFont(f);
+		button3.setBackground(Color.WHITE);
+		panel.add(button3);
+		button3.setBounds(300, 350, 70, 35);
+		button3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Execute when button is pressed
+				System.out.println("Let's do Reset");
+			}
+		});
 
-        JButton button = new JButton("Start");
-        panel.setLayout(null);
-        panel.add(button);
-        button.setBounds(300, 250, 70, 35);
-        button.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                // Execute when button is pressed
-                System.out.println("Let's start");
-            }
-        });
+		JButton button4 = new JButton("Exit");
+		button4.setFont(f);
+		button4.setBackground(Color.WHITE);
+		panel.add(button4);
+		button4.setBounds(300, 400, 70, 35);
+		button4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Execute when button is pressed
+				System.exit(0);
+			}
+		});
 
-        JButton button2 = new JButton("Map");
-        panel.add(button2);
-        button2.setBounds(300, 300, 70, 35);
-        button2.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                // Execute when button is pressed
-                System.out.println("Show map");
-                final JFrame mapFrame = new JFrame("Map");
-                mapFrame.setSize(400, 500);
-                mapFrame.setVisible(true);
-                mapFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-                mapFrame.addWindowListener(new WindowAdapter()
-                {
-                    public void windowClosing(WindowEvent e)
-                    {
-                        mapFrame.dispose();
-                    }
-                });
+		JComboBox startlocation = new JComboBox();
+		startlocation.setBackground(Color.WHITE);
+		startlocation.setForeground(Color.BLUE);
+		startlocation.setFont(f);
+		startlocation.addItem("Prdoo");
+		startlocation.addItem("Rose-Hulmanistan");
+		startlocation.addItem("Chiberia");
+		startlocation.addItem("Los Devilenes");
+		startlocation.addItem("Cashville");
+		startlocation.addItem("China Garden");
+		startlocation.addItem("Fish Girl Statue");
+		startlocation.addItem("Wacdonal's");
+		startlocation.addItem("Maj Tahal");
+		startlocation.addItem("Red Black Tree Park");
+		startlocation.addItem("ARA");
+		panel.add(startlocation);
+		startlocation.setBounds(20, 20, 150, 30);
 
-            }
-        });
+		JComboBox endLocation = new JComboBox();
+		endLocation.setForeground(Color.BLUE);
+		endLocation.setFont(f);
+		endLocation.setBackground(Color.WHITE);
+		endLocation.addItem("Prdoo");
+		endLocation.addItem("Rose-Hulmanistan");
+		endLocation.addItem("Chiberia");
+		endLocation.addItem("Los Devilenes");
+		endLocation.addItem("Cashville");
+		endLocation.addItem("China Garden");
+		endLocation.addItem("Fish Girl Statue");
+		endLocation.addItem("Wacdonal's");
+		endLocation.addItem("Maj Tahal");
+		endLocation.addItem("Red Black Tree Park");
+		endLocation.addItem("ARA");
+		panel.add(endLocation);
+		endLocation.setBounds(200, 20, 150, 30);
 
-        JButton button3 = new JButton("Reset");
-        panel.add(button3);
-        button3.setBounds(300, 350, 70, 35);
-        button3.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                // Execute when button is pressed
-                System.out.println("Let's do Reset");
-            }
-        });
+		JComboBox options = new JComboBox();
+		options.setForeground(Color.BLUE);
+		options.setBackground(Color.WHITE);
+		options.setFont(f);
+		options.addItem("Cost By Distance");
+		options.addItem("Cost By Time");
+		panel.add(options);
+		options.setBounds(20, 250, 150, 30);
 
-        JButton button4 = new JButton("Exit");
-        panel.add(button4);
-        button4.setBounds(300, 400, 70, 35);
-        button4.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                // Execute when button is pressed
-                System.exit(0);
-            }
-        });
+		myFrame.setVisible(true);
 
-        JComboBox startlocation = new JComboBox();
-        startlocation.addItem("a");
-        startlocation.addItem("b");
-        startlocation.addItem("c");
-        startlocation.addItem("d");
-        panel.add(startlocation);
-        startlocation.setBounds(20, 20, 150, 30);
+	}
 
-        JComboBox endLocation = new JComboBox();
-        endLocation.addItem("a");
-        endLocation.addItem("b");
-        endLocation.addItem("c");
-        endLocation.addItem("d");
-        panel.add(endLocation);
-        endLocation.setBounds(200, 20, 150, 30);
-
-        JComboBox options = new JComboBox();
-        options.addItem("a");
-        options.addItem("b");
-        options.addItem("c");
-        options.addItem("d");
-        panel.add(options);
-        options.setBounds(20, 250, 150, 30);
-
-        myFrame.setVisible(true);
-
-    }
-
+	
+	
+	
 
 }
