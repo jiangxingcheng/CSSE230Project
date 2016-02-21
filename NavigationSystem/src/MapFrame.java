3bcc445afbc0d124200a3390cdfad455ac57360e
jiangxingcheng <jiangx1@rose-hulman.edu>
2016-02-21 13:56:03 -0500

    Update MapFrame.java

--- NavigationSystem/src/MapFrame.java
+++ NavigationSystem/src/MapFrame.java
@@ -1,21 +1,26 @@
-import java.awt.*;
+import static java.awt.Color.BLACK;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import static java.awt.Color.*;
-
 public class MapFrame extends JFrame{
-	locationMapHash hash;
-	
+	public List<String> lightedCities;
+	public String startcity;
+	public String endcity;
+	public locationMapHash hash; 
 	public void startWindow(locationMapHash hash) {
 		this.hash = hash;
 		JFrame mapFrame = new JFrame("Map");
@@ -32,12 +37,12 @@ public class MapFrame extends JFrame{
 		});
 	}
 	public class city extends JPanel{
-		
+	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		try
 		{
-			BufferedImage image = ImageIO.read(new File("./img/bg.png"));
+			BufferedImage image = ImageIO.read(new File("src\\bg.png"));
 			g.drawImage(image, 0, 0, this);
 		} catch (IOException e)
 		{
@@ -51,15 +56,34 @@ public class MapFrame extends JFrame{
 			Ellipse2D.Double Circle = new Ellipse2D.Double(ml.xCoordinate, ml.yCoordinate, 15, 15);
 			g2d.setPaint(new Color(3, 210, 0));
 			g2d.fill(Circle);
+			
 			g2d.setPaint(new Color(0));
 			g2d.draw(Circle);
-
+			
 			g2d.setFont(new Font("Times New Roman", Font.BOLD, 12));
 			for(String relation: ml.relations) {
 				g2d.drawLine((int) ml.getX() + 5, (int) ml.getY() + 5, (int) hash.get(relation).getX() + 5, (int) hash.get(relation).getY() + 5);
 			}
 			g2d.drawString(ml.getName(), (int) ml.getX(), (int) ml.getY());
 		}
+		
+		System.out.println(startcity+" "+ endcity+" " + hash);
+		
+		lightedCities =hash.startingDistance(hash.get(startcity),hash.get(endcity)).subList(1,hash.startingDistance(hash.get(startcity),hash.get(endcity)).size());
+		for(String redcities: lightedCities){
+			double x = hash.get(redcities).getX();
+			double y = hash.get(redcities).getY();
+			Ellipse2D.Double Circle2 = new Ellipse2D.Double(x, y, 15, 15);
+			for(String relation: lightedCities) {
+				g2d.setColor(Color.RED);
+				g2d.drawLine((int) x + 5, (int) y + 5, (int) hash.get(relation).getX() + 5, (int) hash.get(relation).getY() + 5);
+			}
+			g2d.setColor(Color.RED);
+			g2d.draw(Circle2);
+			g2d.fill(Circle2);
+			
+		}
+		
 
 	
 	}
