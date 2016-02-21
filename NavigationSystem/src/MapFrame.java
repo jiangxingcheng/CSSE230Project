a0e0122e619a4043dfdd91cb71086f4173550c4f
tdiltdil27 <dilta@rose-hulman.edu>
2016-02-21 15:13:28 -0500

    Update MapFrame.java

--- NavigationSystem/src/MapFrame.java
+++ NavigationSystem/src/MapFrame.java
@@ -1,5 +1,6 @@
 import static java.awt.Color.BLACK;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
@@ -7,6 +8,7 @@ import java.awt.Graphics2D;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -21,6 +23,8 @@ public class MapFrame extends JFrame{
 	public String startcity;
 	public String endcity;
 	public locationMapHash hash; 
+	public int CircleSize = 15;
+	
 	public void startWindow(locationMapHash hash) {
 		this.hash = hash;
 		JFrame mapFrame = new JFrame("Map");
@@ -42,7 +46,7 @@ public class MapFrame extends JFrame{
 		super.paintComponent(g);
 		try
 		{
-			BufferedImage image = ImageIO.read(new File("src\\bg.png"));
+			BufferedImage image = ImageIO.read(new File("src\\img\\bg.png"));
 			g.drawImage(image, 0, 0, this);
 		} catch (IOException e)
 		{
@@ -62,25 +66,44 @@ public class MapFrame extends JFrame{
 			
 			g2d.setFont(new Font("Times New Roman", Font.BOLD, 12));
 			for(String relation: ml.relations) {
-				g2d.drawLine((int) ml.getX() + 5, (int) ml.getY() + 5, (int) hash.get(relation).getX() + 5, (int) hash.get(relation).getY() + 5);
+				g2d.drawLine((int) ml.getX() + 7, (int) ml.getY() + 7, (int) hash.get(relation).getX() + 7, (int) hash.get(relation).getY() + 7);
 			}
 			g2d.drawString(ml.getName(), (int) ml.getX(), (int) ml.getY());
 		}
 		
 		System.out.println(startcity+" "+ endcity+" " + hash);
 		
-		lightedCities =hash.startingDistance(hash.get(startcity),hash.get(endcity)).subList(1,hash.startingDistance(hash.get(startcity),hash.get(endcity)).size());
-		for(String redcities: lightedCities){
-			double x = hash.get(redcities).getX();
-			double y = hash.get(redcities).getY();
-			Ellipse2D.Double Circle2 = new Ellipse2D.Double(x, y, 15, 15);
-			for(String relation: lightedCities) {
-				g2d.setColor(Color.RED);
-				g2d.drawLine((int) x + 5, (int) y + 5, (int) hash.get(relation).getX() + 5, (int) hash.get(relation).getY() + 5);
-			}
+		lightedCities = hash.startingDistance(hash.get(startcity),hash.get(endcity)).subList(1,hash.startingDistance(hash.get(startcity),hash.get(endcity)).size());
+		
+		int counter = 0;
+		for(String redCity: lightedCities){
+			
+			double x = hash.get(redCity).getX();
+			double y = hash.get(redCity).getY();
 			g2d.setColor(Color.RED);
+			
+			if(counter == 0)
+			{
+				g2d.drawString("Start", (int) x - CircleSize, (int) y + 2 * CircleSize);
+			}
+			
+			
+			Ellipse2D.Double Circle2 = new Ellipse2D.Double(x, y, CircleSize, CircleSize);
+			
+			if(counter != 0)
+			{
+				g2d.setStroke(new BasicStroke(5));
+				g2d.drawLine((int) x + 7, (int) y + 7, (int) hash.get(lightedCities.get(counter-1)).getX() + 5, (int) hash.get(lightedCities.get(counter-1)).getY() + 5);
+			}
+			g2d.setStroke(new BasicStroke(1));
 			g2d.draw(Circle2);
 			g2d.fill(Circle2);
+			
+			if(counter == lightedCities.size())
+			{
+				g2d.drawString("Destination", (int) x - CircleSize, (int) y + 2 * CircleSize);
+			}
+			counter++;
 			
 		}
 		
